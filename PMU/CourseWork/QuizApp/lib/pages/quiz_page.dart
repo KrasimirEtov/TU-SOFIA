385d4d28e008b289ae12de1d267caf4652988e93
Krasimir Etov <4341025+KrasimirEtov@users.noreply.github.com>
2019-05-27 00:29:09 +0300

    Make app visible?

---
+++ PMU/CourseWork/QuizApp/lib/pages/quiz_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import '../utils/question.dart';
+import '../utils/quiz.dart';
+
+import '../UI/answer_button.dart';
+import '../UI/question_text.dart';
+import '../UI/correct_wrong_overlay.dart';
+
+import './score_page.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  State createState() => new QuizPageState();
+}
+
+class QuizPageState extends State<QuizPage> {
+
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Мусала е най-високият връх в България?.", true),
+    new Question("Най-източната част на България е нос Шабла", false),
+    new Question("България е създадена през 681 г.", true),
+    new Question("Хан Омуртаг е написал първите писани закони?", false),
+    new Question("Границите на България са били на четири морета?", false),
+    new Question("Мотото на ТУ-София е 'Ние успяваме'?", true)
+  ]);
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayShouldBeVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer) {
+    isCorrect = (currentQuestion.answer == answer);
+    quiz.answer(isCorrect);
+    this.setState(() {
+      overlayShouldBeVisible = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        new Column( // This is our main page
+          children: <Widget>[
+            new AnswerButton(true, () => handleAnswer(true)), //true button
+            new QuestionText(questionText, questionNumber),
+            new AnswerButton(false, () => handleAnswer(false)), // false button
+          ],
+        ),
+        overlayShouldBeVisible == true ? new CorrectWrongOverlay(
+          isCorrect,
+          () {
+            if (quiz.length == questionNumber) {
+              Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (BuildContext context) => new ScorePage(quiz.score, quiz.length)), (Route route) => route == null);
+              return;
+            }
+            currentQuestion = quiz.nextQuestion;
+            this.setState(() {
+              overlayShouldBeVisible = false;
+              questionText = currentQuestion.question;
+              questionNumber = quiz.questionNumber;
+            });
+          }
+        ) : new Container()
+      ],
+    );
+  }
+}
